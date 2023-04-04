7660947f86fc3db28386017e9c04ebc3b5fc3736
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-04-04 10:32:58 +0200

    Don't query a user email/password automatically

--- src/entity/Account.ts
+++ src/entity/Account.ts
@@ -14,12 +14,16 @@ export class Account {
     /**
      * The hashed password of the account.
      */
-    @Column()
+    @Column({
+        select: false, // Don't select the password when querying the database
+    })
     password: string;
 
     /**
      * The email of the account.
      */
-    @Column()
+    @Column({
+        select: false, // Don't select the email when querying the database
+    })
     email: string;
 }
