5873de367bdd970f31cfd11bd2d9e9a1bde3db87
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-04-04 16:14:14 +0200

    Add the /friend route to the main router

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import child_process from 'child_process';
 
-if(process.env.ENV === 'dev') {
+if(process.env.ENV !== 'prod') {
     console.warn('WARNING: Running in development mode.');
     console.warn('WARNING: Using this mode in production CAN CAUSE DATA LOSS !!!');
     console.warn('WARNING: If this is a production environment, please set the ENV variable to "prod" in the .env file.');
@@ -16,6 +16,7 @@ import express, { Express } from "express";
 import * as bodyParser from "body-parser";
 
 import { UserRouter } from "./routes/UserRouter";
+import { FriendRouter } from './routes/FriendRouter';
 
 AppDataSource.initialize().then(async () => {
     console.log('Database connection established');
@@ -26,6 +27,7 @@ AppDataSource.initialize().then(async () => {
 
     // Initialize the routes
     app.use('/user', UserRouter);
+    app.use('/friend', FriendRouter);
 
     // Start the server
     app.listen(process.env.PORT, () => {
