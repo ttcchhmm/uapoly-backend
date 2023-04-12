229b590903673773ab3cff62db3131462a7bd8b4
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-04-12 11:04:40 +0200

    Revert "Remove useless save call"
    This reverts commit ddcf79b1944515a2dd7fd091b028945b00a8092a.

--- src/routes/GameRouter.ts
+++ src/routes/GameRouter.ts
@@ -54,6 +54,8 @@ GameRouter.post('/create', authenticateRequest, async (req: AuthenticatedRequest
     board.jackpot = 0;
     board.salary = req.body.salary;
 
+    await boardRepo.save(board);
+
     // TODO: Allow for custom slots and/or other default sets.
     board.slots = AmericanSlots.map((slot) => {
         slot.board = board;
