e9c2d92587b679a4477bcd13998400e8aad5ca29
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-04-18 14:42:22 +0200

    Fix freeze on non-Arch Linux systems

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+import fs from "fs";
+
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DB_HOST || "127.0.0.1",
@@ -13,13 +15,38 @@ export const AppDataSource = new DataSource({
     entities: ["src/entity/*.{js,ts}"],
     migrations: ["src/migrations/*.{js,ts}"],
     subscribers: [],
-    cache: {
-        type: "redis",
-        options: {
-            host: process.env.REDIS_HOST || "127.0.0.1",
-            port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
-            password: process.env.REDIS_PASSWORD || "",
-        },
-        ignoreErrors: true,
-    }
+    cache: getCacheConfig(),
 });
+
+/**
+ * Get the cache config for TypeORM. Used as a workaround for a bug in TypeORM around non-Arch Linux systems.
+ * @returns false if caching should be disabled, or a TypeORM cache config object.
+ */
+function getCacheConfig(): any {
+    const pacmanFound = fs.existsSync('/usr/bin/pacman');
+    const force = parseInt(process.env.FORCE_CACHE || '0');
+
+    if(pacmanFound) {
+        console.log('Arch-based system detected. Using Redis for caching.');
+    } else if(force) {
+        console.log('WARNING: Force enabling database caching on known broken configuration.')
+        console.log('WARNING: Database caching is known to be broken on non-Arch Linux systems.');
+        console.log('WARNING: Set FORCE_CACHE=0 to disable caching.');
+    }
+
+    if(pacmanFound || force) {
+        return {
+            type: "redis",
+            options: {
+                host: process.env.REDIS_HOST || "127.0.0.1",
+                port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+                password: process.env.REDIS_PASSWORD || "",
+            },
+            ignoreErrors: true,
+        }
+    } else {
+        console.log('WARNING: Database caching is known to be broken on non-Arch Linux systems.');
+        console.log('WARNING: Set FORCE_CACHE=1 to enable caching anyway.');
+        return false;
+    }
+}
