f3fb66522558f79c6d1a82dac08d145c98c69258
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-04-20 13:14:31 +0200

    Implement a basic state machine

---
+++ src/state/State.ts
@@ -0,0 +1,106 @@
+import { StateMachine } from "./StateMachine";
+
+/**
+ * A state in a state machine.
+ * 
+ * @template T An enum of all the possible transitions that can occur.
+ * @template N An enum of all the possible states that can occur.
+ */
+export class State<T, N> {
+    /**
+     * The name of the state.
+     */
+    private name: N;
+
+    /**
+     * The transitions that can be made to this state.
+     */
+    private inTransitions: T[];
+
+    /**
+     * The transitions that can be made from this state.
+     */
+    private outTransitions: T[];
+
+    /**
+     * The functions to be called when entering this state.
+     */
+    private onEnter: Array<(machine: StateMachine<T, N>, event: T) => void>;
+
+    /**
+     * The functions to be called when exiting this state.
+     */
+    private onExit: Array<(machine: StateMachine<T, N>, event: T) => N>;
+
+    /**
+     * The state machine that this state is a part of.
+     */
+    private machine: StateMachine<T, N>;
+
+    /**
+     * Constructs a new state.
+     * @param name The name of the state.
+     * @param inTransitions The transitions that can be made to this state.
+     * @param outTransitions The transitions that can be made from this state.
+     * @param enterFunctions The functions to be called when entering this state.
+     * @param exitFunctions The functions to be called when exiting this state.
+     */
+    constructor(name: N, inTransitions: T[], outTransitions: T[], enterFunctions: Array<(machine: StateMachine<T, N>, event: T) => void>, exitFunctions: Array<(machine: StateMachine<T, N>, event: T) => N>) {
+        this.name = name;
+        this.inTransitions = inTransitions;
+        this.outTransitions = outTransitions;
+        this.onEnter = enterFunctions;
+        this.onExit = exitFunctions;
+    }
+
+    /**
+     * Set the parent machine of this state.
+     * @param machine The state machine that this state is a part of.
+     */
+    public setParentMachine(machine: StateMachine<T, N>) {
+        this.machine = machine;
+    }
+
+    /**
+     * Enter this state.
+     * @param event The event to transition on.
+     */
+    public enter(event: T) {
+        this.onEnter.forEach((func) => func(this.machine, event));
+    }
+
+    /**
+     * Exit this state.
+     * @param event The event to transition on.
+     * @returns The name of the next state.
+     */
+    public exit(event: T): N {
+        let nextState: N;
+        this.onExit.forEach((func) => nextState = func(this.machine, event));
+        return nextState;
+    }
+
+    /**
+     * Get the transitions that can be made to this state.
+     * @returns The transitions that can be made to this state.
+     */
+    public getInTransitions(): T[] {
+        return this.inTransitions;
+    }
+
+    /**
+     * Get the transitions that can be made from this state.
+     * @returns The transitions that can be made from this state.
+     */
+    public getOutTransitions(): T[] {
+        return this.outTransitions;
+    }
+
+    /**
+     * Get the name of this state.
+     * @returns The name of this state.
+     */
+    public getName(): N {
+        return this.name;
+    }
+}
---
+++ src/state/StateMachine.ts
@@ -0,0 +1,84 @@
+import { State } from "./State";
+
+/**
+ * Defines a state machine.
+ * 
+ * @template T An enum of all the possible transitions that can occur.
+ * @template N An enum of all the possible states that can occur.
+ */
+export class StateMachine<T, N> {
+    /**
+     * The current state of the state machine.
+     */
+    private currentState: State<T, N>;
+
+    /**
+     * An array of all the states in the state machine.
+     */
+    private states: State<T, N>[];
+
+    /**
+     * Whether or not to throw an error when an invalid transition is attempted.
+     */
+    private throwOnInvalidTransition: boolean;
+
+    /**
+     * Constructs a new state machine.
+     * @param initialState The initial state of the state machine.
+     * @param states The states of the state machine.
+     * @param throwOnInvalidTransition Whether or not to throw an error when an invalid transition is attempted.
+     */
+    constructor(initialState: N, states: State<T, N>[], throwOnInvalidTransition = true) {
+        this.states = states;
+        this.throwOnInvalidTransition = throwOnInvalidTransition;
+
+        // Set the parent machine of each state
+        this.states.forEach((state) => state.setParentMachine(this));
+
+        // Get the initial state
+        this.currentState = states.find((state) => state.getName() === initialState);
+
+        // Check if initial state is defined
+        if (this.currentState === undefined) {
+            throw new Error(`State ${initialState} is not defined.`);
+        }
+
+        // Enter the initial state
+        this.currentState.enter(undefined);
+    }
+
+    /**
+     * Transitions the state machine to the next state.
+     * @param event The event to transition on.
+     */
+    public transition(event: T) {
+        // Check out transitions of current state
+        if (!this.currentState.getOutTransitions().includes(event) && this.throwOnInvalidTransition) {
+            throw new Error(`Invalid transition from ${this.currentState.getName()} with event ${event}`);
+        } else {
+            // Get the new state
+            const newState = this.states.find((state) => state.getName() === this.currentState.exit(event));
+
+            // Check if new state is defined
+            if(this.currentState === undefined) {
+                throw new Error(`State ${this.currentState.getName()} is not defined.`);
+            }
+
+            // Check in transitions of new state
+            if(!newState.getInTransitions().includes(event) && this.throwOnInvalidTransition) {
+                throw new Error(`Invalid transition from ${this.currentState.getName()} to ${newState.getName()} with event ${event}`);
+            } else {
+                this.currentState = newState;
+                this.currentState.enter(event);
+            }
+        }
+    }
+
+    /**
+     * Gets the current state of the state machine.
+     * @returns The current state of the state machine.
+     */
+    public getCurrentState(): N {
+        return this.currentState.getName();
+    }
+}
